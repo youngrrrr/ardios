08b151627d74419f1533bcb923dac334c4cb5152
youngrrrr <young-rae_kim@brown.edu>
2015-11-28 02:36:14 -0500

    renamed folders in public to shorter versions

---
+++ public/js/authenticate.js
@@ -0,0 +1,13 @@
+console.log("Hi!")
+
+// $("#authenticate-rdio").click(function(){
+// 	R.authenticate({
+// 		complete: function(authenticated){
+// 			if(authenticated) {
+// 				console.log("User authenticated");
+// 				authenticationComplete();
+// 			}
+// 		},
+// 		mode: 'redirect'
+// 	})
+// });
