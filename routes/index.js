d9ae239c8e5ab2d210f4c1995d030814becb5513
youngrrrr <young-rae_kim@brown.edu>
2015-11-28 02:34:43 -0500

    Replaced Jade template with html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+// var path = require('path');
+
+var PROJECT_DIRECTORY = process.env.PWD;
+
+/* TODO: Jade templating? */
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(PROJECT_DIRECTORY + '/views/index.html');
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
