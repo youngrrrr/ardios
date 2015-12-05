56f823131b703caca686e4962a34a6cbca13c2ad
youngrrrr <young-rae_kim@brown.edu>
2015-12-04 19:56:58 -0500

    Client verification now OK! Still need to figure out what to do with the tokens though.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,44 @@
 var express = require('express');
 var router = express.Router();
-// var path = require('path');
+var path = require('path');
+var buffer = require('buffer');
+var request = require('request');
+var keys = require('../private/keys.js');
 
-var PROJECT_DIRECTORY = process.env.PWD;
+//var PROJECT_DIRECTORY = process.env.PWD;
+var REDIRECT_URI = 'http://localhost:3000/rdio-code';
+var RDIO_ID = 'pz3i3g4smfdofidsegap6d5c3y';
+var RDIO_SECRET = keys.RDIO_SECRET; // Private
 
 /* TODO: Jade templating? */
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(PROJECT_DIRECTORY + '/views/index.html');
-  // res.render('index', { title: 'Express' });
+  res.sendFile(path.join(process.env.PWD, 'views', 'index.html'));
+});
+
+router.get('/rdio-code', function(req, res, next) {
+  var code = req.query.code;
+
+  var rdioIdSecretAuth = 'Basic ' + new Buffer(
+  	RDIO_ID + ':' + RDIO_SECRET).toString('base64');
+
+  request.post({
+  	url: 'https://services.rdio.com/oauth2/token',
+  	headers: {
+  		'Authorization': rdioIdSecretAuth
+  	},
+  	form: {
+  		'grant_type': 'authorization_code',
+  		'code': code,
+  		'redirect_uri': REDIRECT_URI
+  	}
+  }, function(error, res, body) {
+  	console.log(res.statusCode);
+  	console.log(body);
+  });
+
+  res.redirect('/');
 });
 
 module.exports = router;
