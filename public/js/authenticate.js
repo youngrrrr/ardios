b523957eb7e94e15f9222e191cc95eb3f545d802
youngrrrr <young-rae_kim@brown.edu>
2015-12-26 04:20:13 -0500

    OAuth 2.0 authentication logic

--- public/js/authenticate.js
+++ public/js/authenticate.js
@@ -1,14 +1,39 @@
-var REDIRECT_URI = "http://localhost:3000/rdio-code";
+/* http://www.codebelt.com/jquery/open-new-browser-window-with-jquery-custom-size/ */
+/* "javascript popup window" */
+
+var REDIRECT_URI = "http://localhost:3000/auth";
 
 $("#authenticate-rdio").click(function() {
-	window.location = "https://www.rdio.com/oauth2/authorize"
+	//window.location
+
+	var authParamsRdio = {
+		response_type: "code",
+		client_id: "pz3i3g4smfdofidsegap6d5c3y",
+		redirect_uri: REDIRECT_URI + "?service=rdio",
+		mode: 'popup',
+		hideSignup: true
+	};
+
+	var authUrlRdio = "https://www.rdio.com/oauth2/authorize"
 		+ "?"
-		+ $.param(
-			{
-				response_type: "code",
-				client_id: "pz3i3g4smfdofidsegap6d5c3y",
-				redirect_uri: REDIRECT_URI,
-				mode: 'popup',
-				hideSignup: true
-			});
+		+ $.param(authParamsRdio);
+
+	window.open(authUrlRdio, "_blank", "width=400, height=500", "false");
+});
+
+$("#authenticate-spotify").click(function() {
+	//window.location
+
+	var authParamsSpotify = {
+		client_id: "3eb36ca7e3274cfb9c23cb59e030fe57",
+		response_type: "code",
+		redirect_uri: REDIRECT_URI + "?service=spotify",
+		scope: "playlist-modify-public playlist-modify-private user-follow-modify user-library-modify"
+	};
+
+	var authUrlSpotify = "https://accounts.spotify.com/authorize"
+		+ "?"
+		+ $.param(authParamsSpotify);
+
+	window.open(authUrlSpotify, "_blank", "width=400, height=500", "false");
 });
--- routes/index.js
+++ routes/index.js
@@ -6,39 +6,67 @@ var request = require('request');
 var keys = require('../private/keys.js');
 
 //var PROJECT_DIRECTORY = process.env.PWD;
-var REDIRECT_URI = 'http://localhost:3000/rdio-code';
+var REDIRECT_URI = 'http://localhost:3000/auth';
 var RDIO_ID = 'pz3i3g4smfdofidsegap6d5c3y';
 var RDIO_SECRET = keys.RDIO_SECRET; // Private
+
+var SPOTIFY_ID = '3eb36ca7e3274cfb9c23cb59e030fe57';
+var SPOTIFY_SECRET = keys.SPOTIFY_SECRET; //Private
 
 /* TODO: Jade templating? */
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(process.env.PWD, 'views', 'index.html'));
+	res.sendFile(path.join(process.env.PWD, 'views', 'index.html'));
 });
 
-router.get('/rdio-code', function(req, res, next) {
-  var code = req.query.code;
+/* OAuth 2.0 */
+router.get('/auth', function(req, res, next) {
+	var service = req.query.service;
+	var code = req.query.code;
 
-  var rdioIdSecretAuth = 'Basic ' + new Buffer(
-  	RDIO_ID + ':' + RDIO_SECRET).toString('base64');
+	var authorizationVal = 'Basic ';
+	var postRequestOptions = {};
 
-  request.post({
-  	url: 'https://services.rdio.com/oauth2/token',
-  	headers: {
-  		'Authorization': rdioIdSecretAuth
-  	},
-  	form: {
-  		'grant_type': 'authorization_code',
-  		'code': code,
-  		'redirect_uri': REDIRECT_URI
-  	}
-  }, function(error, res, body) {
-  	console.log(res.statusCode);
-  	console.log(body);
-  });
+	if (service == 'rdio') {
+		authorizationVal += new Buffer(RDIO_ID + ':' + RDIO_SECRET).toString('base64');
 
-  res.redirect('/');
+		postRequestOptions = {
+			url: 'https://services.rdio.com/oauth2/token',
+			headers: {
+				'Authorization': authorizationVal
+			},
+			form: {
+				'grant_type': 'authorization_code',
+				'code': code,
+				'redirect_uri': REDIRECT_URI + '?service=rdio'
+			}
+		};
+	}
+	else if (service == 'spotify') {
+		authorizationVal += new Buffer(SPOTIFY_ID + ':' + SPOTIFY_SECRET).toString('base64');
+
+		postRequestOptions = {
+			url: 'https://accounts.spotify.com/api/token',
+			headers: {
+				'Authorization': authorizationVal
+			},
+			form: {
+				'grant_type': 'authorization_code',
+				'code': code,
+				'redirect_uri': REDIRECT_URI + '?service=spotify'
+			}
+		};
+	}
+	else {
+		console.log('none');
+		return;
+	}
+
+	request.post(postRequestOptions, function(err, res, body) {
+		console.log(res.statusCode);
+		console.log(body);
+	});
 });
 
 module.exports = router;
