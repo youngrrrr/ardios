a3cf450ab74759525b2159c1bdc445a17e9cbea2
youngrrrr <young-rae_kim@brown.edu>
2016-03-31 01:21:55 +0900

    Functionality to save songs by track ID added. Input goes in textarea: first value must be access token from authenticating spotify. Sequential values are track IDs of songs to be added. All values must be separated by a line break.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var buffer = require('buffer');
 var request = require('request');
 var keys = require('../private/keys.js');
 
+var Promise = require('bluebird');
+
 //var PROJECT_DIRECTORY = process.env.PWD;
 var REDIRECT_URI = 'http://localhost:3000/auth';
 var RDIO_ID = 'pz3i3g4smfdofidsegap6d5c3y';
@@ -21,7 +23,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* OAuth 2.0 */
-router.get('/auth', function(req, res, next) {
+router.get('/auth', Promise.coroutine(function* (req, res, next) {
 	var service = req.query.service;
 	var code = req.query.code;
 
@@ -63,10 +65,78 @@ router.get('/auth', function(req, res, next) {
 		return;
 	}
 
-	request.post(postRequestOptions, function(err, res, body) {
-		console.log(res.statusCode);
-		console.log(body);
-	});
-});
+  var token = yield getSpotifyAccessToken(postRequestOptions);
+
+  res.status(200).send(token);
+}));
+
+function getSpotifyAccessToken(postRequestOptions) {
+  return new Promise(function(resolve, reject) {
+    request.post(postRequestOptions, function(err, res, body) {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      var parsedBody = JSON.parse(body);
+      resolve(parsedBody.access_token);
+    });
+  });
+}
+
+router.route('/saved')
+  .post(Promise.coroutine(addTracksToSaved));
+
+const MAX_IDS = 50;
+
+function* addTracksToSaved(req, res) {
+  var tracksStr = req.body.trackIds.split('\r\n');
+  var tracksStrLength = tracksStr.length;
+
+  var spotifyAccessToken = tracksStr[0];
+
+  var batchCounter = 0;
+  var batchedTracksStr = '';
+
+  for (var i = 1; i < tracksStrLength; i++) {
+    batchedTracksStr += tracksStr[i] + ',';
+    batchCounter++;
+    if (batchCounter === MAX_IDS) {
+      yield putSavedTracks(i, batchedTracksStr, spotifyAccessToken);
+      batchedTracksStr = '';
+      batchCounter = 0;
+    }
+  }
+
+  yield putSavedTracks(i, batchedTracksStr, spotifyAccessToken);
+
+  res.status(200).send('Done.');
+}
+
+function putSavedTracks(i, tracksStr, accessToken) {
+  return new Promise(function(resolve, reject) {
+    console.log(i);
+    request({
+      method: 'PUT',
+      uri: 'https://api.spotify.com/v1/me/tracks?ids=' + tracksStr,
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    },
+    function (error, response, body) {
+      if (error) {
+        console.error('upload failed:', error)
+        reject(error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.log(tracksStr, response, body);
+      }
+
+      resolve();
+    });
+  });
+}
 
 module.exports = router;
